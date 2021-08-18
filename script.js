83c8ddd315e1d4278e84077c1cba8ff837943288
Sivan Kaplan <sivanagar@gmail.com>
2021-08-18 14:51:50 -0700

    Verify password length input is length

--- script.js
+++ script.js
@@ -4,24 +4,9 @@ let charsSet = {
   upperCase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
   lowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
   numeric: ["0","1","2","3","4","5","6","7","8","9"],
-  special: ["!","@","#","%","^","&","*","(",")"]
+  special: ["!","@","#","%","^","&","*","(",")","$","+",",","-","_",".",":",";","=","?","[","]","|","~","{","}"]
 };
-//Set variable with all passwors requirments
-let passwordReq ={
-  length: 8,
-  upperCase: true,
-  lowerCase: true,
-  numeric: true,
-  special: true,
-}
-// Getting info from user
-var getInfo = function () {
-passwordReq.length = window.prompt("What is the length of the password required? Please enter a number betwee 8 to 128.");
-passwordReq.upperCase = window.confirm("Would you like UPPERCASE characters in your password?");
-passwordReq.lowerCase = window.confirm("Would you like lowercase characters in your password?");
-passwordReq.numeric = window.confirm("Would you like Numeric characters in your password?");
-passwordReq.special = window.confirm("Would you like Special characters in your password? Special characters are");
-}
+
 // get random characters
 var getRandom = function (char) {
   return char[Math.floor(Math.random()*char.length)];
@@ -36,36 +21,54 @@ function shuffle(array) {
 }
 
 var generatePassword = function () {
-  getInfo();
-  
   let password = [""]
   let i = 0;
-  charsSet.fullList = [];
-  debugger;
-  if (passwordReq.lowerCase) {
-    password[i]= getRandom(charsSet.lowerCase);
-    charsSet.fullList = charsSet.fullList.concat(charsSet.lowerCase);
-    i++;
+  charsSet.fullList = []; //this will contain the full list of possible characters
+  // Getting length from user and validating it
+  var passwordLength = 0; 
+  while (passwordLength == "" || passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    passwordLength = window.prompt("What is the length of the password required? Please enter a number betwee 8 to 128.");
   }
-  if (passwordReq.upperCase) {
-    password[i]= getRandom(charsSet.upperCase);
-    charsSet.fullList = charsSet.fullList.concat(charsSet.upperCase);
-    i++;
+  
+  while (i == 0){
+    //Asking to confirm which charcters types
+    if (window.confirm("Would you like lowercase characters in your password?")) {
+      //add at least 1 random char to the password
+      password[i]= getRandom(charsSet.lowerCase);
+      //add all lower case to the array of possible chars
+      charsSet.fullList = charsSet.fullList.concat(charsSet.lowerCase);
+      i++;
+    }
+    //follow with other types of characters
+    if (window.confirm("Would you like UPPERCASE characters in your password?")) {
+      password[i]= getRandom(charsSet.upperCase);
+      charsSet.fullList = charsSet.fullList.concat(charsSet.upperCase);
+      i++;
+    }
+    if (window.confirm("Would you like Numeric characters in your password?")) {
+      password[i]= getRandom(charsSet.numeric);
+      charsSet.fullList = charsSet.fullList.concat(charsSet.numeric);
+      i++
+    }
+    if (window.confirm("Would you like Special characters in your password? Special characters are " + charsSet.special.join(''))) {
+      password[i]= getRandom(charsSet.special);
+      charsSet.fullList = charsSet.fullList.concat(charsSet.special);
+      i++
+    }
+    if (i == 0){
+      alert("You MUST confirm at least one type of characters in your password!")
+    }
   }
-  if (passwordReq.numeric) {
-    password[i]= getRandom(charsSet.numeric);
-    charsSet.fullList = charsSet.fullList.concat(charsSet.numeric);
-    i++
-  }
-  if (passwordReq.special) {
-    password[i]= getRandom(charsSet.special);
-    charsSet.fullList = charsSet.fullList.concat(charsSet.special);
-    i++
-  }
-  for (i ; i < passwordReq.length; i++) {
+
+  // filling the remained characters of password with random character
+  for (i ; i < passwordLength; i++) {
     password[i] = getRandom(charsSet.fullList);
   }
+
+  //Shuffeling the charcters order of the password
   password = shuffle(password);
+
+  //returning the password as string and not array
   return (password.join(''));
 }
 
